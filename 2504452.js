3bd881d5cb633ef3eca2715f80743849c84b541c
DM-fin <denismil@uef.fi>
2026-03-19 13:56:50 +0200

    Add files via upload

--- 2504452.js
+++ 2504452.js
@@ -7,10 +7,12 @@ function toggleScanner() {
     if (scannerOn) {
         startScanner();
         mapContainer.style.display = "none";
+        inventory.style.display = "none";
         btn.innerText = "Cancel";
     } else {
         stopScanner();
         mapContainer.style.display = "block";
+        inventory.style.display = "flex";
         btn.innerText = "Scan";
     }   
         // Start scanning logic here
@@ -21,9 +23,12 @@ function startScanner() {
         { facingMode: "environment" },
         {},
         function (text) {
-            // console.log(text);
+            console.log(text);
             const place = JSON.parse(text);
-            showMarkerAt(place.top, place.left);    
+            showMarkerAt(place.top, place.left);
+            document.getElementById("name").innerText = 'Item name: ' + place.name;
+            document.getElementById("price").innerHTML = 'Price (&#x20AC): ' + place.price;
+            document.getElementById("inStock").innerText = 'In Stock: ' + Number(place.inStock);
             toggleScanner();
         }
     ).catch(function (err) {
